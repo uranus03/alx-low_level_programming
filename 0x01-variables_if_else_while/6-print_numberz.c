bd6938daeccc2ea64c55614b3f02eb1388e5f116
uranus03 <hajarisser@gmail.com>
2023-04-24 15:57:30 +0200

    task0

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + 48);
+	}
+	putchar('\n');
+	return (0);
+}
+

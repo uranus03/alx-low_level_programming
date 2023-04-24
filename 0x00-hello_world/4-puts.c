8c5d2e7af686d6882f16d5ad58e49eca63149cef
uranus03 <hajarisser@gmail.com>
2023-04-24 09:36:11 +0200

    task0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main - entry poit
+* main - entry point
 * return : always 0 (success)
 */
 int main(void)
-{
+{ /* Notice that final new line character is implicit */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }

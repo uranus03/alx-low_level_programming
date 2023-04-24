00741238d191cc1429e9458feace2cf4f8dd5599
uranus03 <hajarisser@gmail.com>
2023-04-24 10:44:58 +0200

    task0

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-*main - entry point
-*return: always 0 for success
-*/
+ *main - entry point
+ *Return: always 0 for success
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n"); 
-	return (0); 
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }

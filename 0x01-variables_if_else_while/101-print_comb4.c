0ca21ebcc0bbc77808d3b1a75a6005f8ab07cd1e
uranus03 <hajarisser@gmail.com>
2023-04-24 21:43:15 +0200

    advenced task

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,17 +9,20 @@ int main(void)
 
 	for (n = 48; n < 58; n++)
 	{
-		for (l = 50; l < 58; l++)
+		for (m = 49; m < 58; m++)
 		{
-			if (l > m && m > n)
+			for (l = 50; l < 58; l++)
 			{
-				putchar(n);
-				putchar(m);
-				putchar(l);
-				if (n != 55 || m != 56)
+				if (l > m && m > n)
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(n);
+					putchar(m);
+					putchar(l);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
 		}

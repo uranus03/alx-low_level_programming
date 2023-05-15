467d7608f52ee99c51ef8f5bede297be46f4a07d
uranus03 <hajarisser@gmail.com>
2023-05-15 14:01:19 +0200

    tasks

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -19,21 +19,22 @@ char *argstostr(int ac, char **av)
 	{
 		for (n = 0; av[i][n]; n++)
 			l++;
-		l += ac;
-		str = malloc(sizeof(char) * l + 1);
-		if (str == NULL)
-			return (NULL);
-		for (i = 0; i < ac; i++)
-		{
-			for (n = 0; av[i][n]; n++)
-			{
-				str[r] = av[i][n];
-				r++;
-			}
-			if (str[r] == '\0')
-			{
-				str[r++] = '\n';
-			}
-		}
-		return (str);
 	}
+	l += ac;
+	str = malloc(sizeof(char) * l + 1);
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n]; n++)
+		{
+			str[r] = av[i][n];
+			r++;
+		}
+		if (str[r] == '\0')
+		{
+			str[r++] = '\n';
+		}
+	}
+	return (str);
+}

5ba52207684c6f41d84bb02f2511247366f1d54f
uranus03 <hajarisser@gmail.com>
2023-05-17 19:55:41 +0200

    tasks

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,7 +1,10 @@
 #include "main.h"
-
+/**
+ *main - main function
+ *Return: = success
+ */
 int main(void)
 {
-	_puts("\"At the end of the day, my goal was to be the best hacker\"\n\t- Kevin Mitnick");
+	_puts("\"At the end of the day,my goal was to be the best hacker\"\n\t- Kevin Mitnick");
 	return (0);
 }
